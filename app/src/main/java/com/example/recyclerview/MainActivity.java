6e603c62065f2282cf27da0e415dd3e2bc01d40d
asif331001 <asif95331001@gmail.com>
2023-03-13 19:55:34 +0600

    fruits in recycler view

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    int[] images = {
+            R.drawable.apple,R.drawable.banana,R.drawable.orange,R.drawable.mango,
+            R.drawable.watermelon,R.drawable.strawberry,R.drawable.grapes,
+            R.drawable.pineapple,R.drawable.pear,R.drawable.peach,
+    };
+
+    String[] title, desc;
+    MyAdapter myAdapter;
 
     RecyclerView recyclerView;
 
@@ -13,8 +23,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         recyclerView = findViewById(R.id.recyclerViewID);
 
+        title = getResources().getStringArray(R.array.fruitsName);
+        desc = getResources().getStringArray(R.array.fruitsShortInfo);
+
+        myAdapter  = new MyAdapter(this,title,desc,images);
+
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
